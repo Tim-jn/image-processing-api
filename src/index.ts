b4250f1771beecc03597be6a620f1c2cccb8f5e7
Tim <Tim280696@hotmail.com>
2022-06-20 21:17:31 +0200

    index: update endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import routes from "./routes/index";
 const app = express();
 const port = 8000;
 
-app.use("/", routes);
+app.use("/api", routes);
 
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`);
