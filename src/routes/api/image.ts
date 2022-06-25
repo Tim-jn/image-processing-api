2975bc5ce6925c5532f2c9057fb0348e577bb3ce
Tim <Tim280696@hotmail.com>
2022-06-25 12:03:20 +0200

    image: fixed

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,26 +1,29 @@
 import express, { Request, Response, NextFunction } from "express";
 import sharp from "sharp";
+import path from "path";
 const images = express.Router();
-
-// type Props = {
-//   req: Request;
-//   res: Response;
-//   next: NextFunction;
-// };
 
 const resizeImage = async (
   req: Request,
   res: Response,
   next: NextFunction
-): Promise<void | string> => {
+): Promise<void> => {
   try {
-    await sharp(`./assets/images/full/${req.query.filename}.jpg`)
+    await sharp(
+      path.resolve(
+        __dirname,
+        `../../../assets/images/full/${req.query.filename}.jpg`
+      )
+    )
       .resize({
         width: parseInt(req.query.width as string),
         height: parseInt(req.query.height as string),
       })
       .toFile(
-        `./assets/images/thumb/${req.query.filename}-${req.query.width}x${req.query.height}.jpg`
+        path.resolve(
+          __dirname,
+          `../../../assets/images/thumb/${req.query.filename}-${req.query.width}x${req.query.height}.jpg`
+        )
       );
   } catch (error) {
     console.log(`An error occurred during processing: ${error}`);
@@ -28,25 +31,19 @@ const resizeImage = async (
   next();
 };
 
-const sendResizedImage = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<any> => {
+images.use(resizeImage);
+
+images.get("/", (req, res) => {
   try {
-    await resizeImage(req, res, next);
     res.sendFile(
-      `C:/Users/Tim28/Desktop/dev/udacity/image-processing-api/assets/images/thumb/${req.query.filename}-${req.query.width}x${req.query.height}.jpg`
+      path.resolve(
+        __dirname,
+        `../../../assets/images/thumb/${req.query.filename}-${req.query.width}x${req.query.height}.jpg`
+      )
     );
   } catch (error) {
     console.log(`An error occurred during processing: ${error}`);
   }
-  next();
-};
-
-images.get("/", (req, res, next) => {
-  resizeImage(req, res, next);
-  sendResizedImage(req, res, next);
 });
 
 export default images;
