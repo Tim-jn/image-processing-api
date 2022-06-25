29455c71cd2db3f7ceb8e7d3c0a3fd3e16cc11e5
Tim <Tim280696@hotmail.com>
2022-06-25 10:56:49 +0200

    Test update

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,8 +1,52 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import sharp from "sharp";
 const images = express.Router();
 
-images.get("/", (req, res) => {
-  res.send("Hello world !");
+// type Props = {
+//   req: Request;
+//   res: Response;
+//   next: NextFunction;
+// };
+
+const resizeImage = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void | string> => {
+  try {
+    await sharp(`./assets/images/full/${req.query.filename}.jpg`)
+      .resize({
+        width: parseInt(req.query.width as string),
+        height: parseInt(req.query.height as string),
+      })
+      .toFile(
+        `./assets/images/thumb/${req.query.filename}-${req.query.width}x${req.query.height}.jpg`
+      );
+  } catch (error) {
+    console.log(`An error occurred during processing: ${error}`);
+  }
+  next();
+};
+
+const sendResizedImage = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    await resizeImage(req, res, next);
+    res.sendFile(
+      `C:/Users/Tim28/Desktop/dev/udacity/image-processing-api/assets/images/thumb/${req.query.filename}-${req.query.width}x${req.query.height}.jpg`
+    );
+  } catch (error) {
+    console.log(`An error occurred during processing: ${error}`);
+  }
+  next();
+};
+
+images.get("/", (req, res, next) => {
+  resizeImage(req, res, next);
+  sendResizedImage(req, res, next);
 });
 
 export default images;
