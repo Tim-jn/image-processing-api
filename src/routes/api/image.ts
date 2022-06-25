48cb6cc3f528a5715e062e30fd7ad7d8f286a2d1
Tim <Tim280696@hotmail.com>
2022-06-25 12:51:43 +0200

    image: update func + add if/else

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -2,7 +2,6 @@ import express, { Request, Response, NextFunction } from "express";
 import sharp from "sharp";
 import path from "path";
 const images = express.Router();
-const imagePath = path.resolve(__dirname, `../../../assets/images/`);
 
 const resizeImage = async (
   req: Request,
@@ -11,16 +10,33 @@ const resizeImage = async (
 ): Promise<void> => {
   const width = parseInt(req.query.width as string);
   const height = parseInt(req.query.height as string);
+  const filename = req.query.filename as string;
+  const imagePath = path.resolve(__dirname, `../../../assets/images/`);
+
+  const dataImages =
+    "encenadaport, fjord, icelandwaterfall, palmtunnel, santamonica";
 
   try {
-    await sharp(`${imagePath}/full/${req.query.filename}.jpg`)
-      .resize({
-        width,
-        height,
-      })
-      .toFile(
-        `${imagePath}/thumb/${req.query.filename}-${req.query.width}x${req.query.height}.jpg`
+    if (filename === undefined || !dataImages.includes(filename)) {
+      res.send(
+        `</span>Please enter a valid <b>filename</b>, for example : <a href='images?filename=encenadaport&width=250&height=250'>api/images?filename=encenadaport&width=250&height=250</a></span><br/> <br/>Here is a list of available filenames : ${dataImages}`
       );
+    } else if (isNaN(width && height)) {
+      res.send(
+        "</span>Please enter a valid number for <b>height</b> and <b>width</b>, for example : <a href='images?filename=encenadaport&width=250&height=250'>api/images?filename=encenadaport&width=250&height=250</a></span>"
+      );
+    } else if (dataImages.includes(filename) && !isNaN(width && height)) {
+      await sharp(`${imagePath}/full/${filename}.jpg`)
+        .resize({
+          width,
+          height,
+        })
+        .toFile(`${imagePath}/thumb/${filename}-${width}x${height}.jpg`);
+
+      images.get("/", (req, res) => {
+        res.sendFile(`${imagePath}/thumb/${filename}-${width}x${height}.jpg`);
+      });
+    }
   } catch (error) {
     console.log(`An error occurred during processing: ${error}`);
   }
@@ -28,15 +44,5 @@ const resizeImage = async (
 };
 
 images.use(resizeImage);
-
-images.get("/", (req, res) => {
-  try {
-    res.sendFile(
-      `${imagePath}/thumb/${req.query.filename}-${req.query.width}x${req.query.height}.jpg`
-    );
-  } catch (error) {
-    console.log(`An error occurred during processing: ${error}`);
-  }
-});
 
 export default images;
