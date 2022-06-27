b87b63d14029a4a809bd3d98cc4cf93157b86251
Tim <Tim280696@hotmail.com>
2022-06-27 20:32:01 +0200

    indexSpec: update tests

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,15 +1,13 @@
 import request from "supertest";
 import { app } from "../index";
 
-describe("Test for index", () => {
-  describe("GET /", () => {
-    it("should return the / endpoint", async () => {
-      const response = await request(app).get("/");
-      expect(response.status).toBe(200);
-    });
-    it("should return the title and the description of the api", async () => {
-      const response = await request(app).get("/");
-      expect(response.text).toMatch("Welcome to the image processing API !");
-    });
+describe("GET /", () => {
+  it("should return the / endpoint", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+  });
+  it("should return the title and the description of the api", async () => {
+    const response = await request(app).get("/");
+    expect(response.text).toMatch("Welcome to the image processing API !");
   });
 });
