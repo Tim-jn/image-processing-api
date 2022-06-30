7f067345ee5eb2cfd4a29d17549e73e400634707
Tim <Tim280696@hotmail.com>
2022-06-30 11:38:04 +0200

    image: update func

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,55 +1,46 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response } from "express";
 import sharp from "sharp";
 import path from "path";
 import { dataImages } from "../../index";
 
 const images = express.Router();
 
-const resizeImage = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
+async function resizeImage(req: Request, res: Response): Promise<void> {
   const width = parseInt(req.query.width as string);
   const height = parseInt(req.query.height as string);
   const filename = req.query.filename as string;
   const imagePath = path.resolve(__dirname, `../../../assets/images/`);
 
-  try {
-    if (filename === undefined || !dataImages.includes(filename)) {
-      res.status(404);
-      res.send(
-        `</span>Input file is missing : please enter a valid <b>filename</b> !<br/> <br/>For example : <a href='images?filename=encenadaport&width=250&height=250'>api/images?filename=encenadaport&width=250&height=250</a></span><br/> <br/>Here is a list of available filenames : ${dataImages}`
-      );
-    } else if (isNaN(width && height)) {
-      res.status(404);
-      res.send(
-        "</span>Please enter a valid number for <b>height</b> and <b>width</b>, for example : <a href='images?filename=encenadaport&width=250&height=250'>api/images?filename=encenadaport&width=250&height=250</a></span>"
-      );
-    } else if (dataImages.includes(filename) && !isNaN(width && height)) {
-      res.status(200);
+  if (filename === undefined || !dataImages.includes(filename)) {
+    res.status(404);
+    res.send(
+      `</span>Input file is missing : please enter a valid <b>filename</b> !<br/> <br/>For example : <a href='images?filename=encenadaport&width=250&height=250'>api/images?filename=encenadaport&width=250&height=250</a></span><br/> <br/>Here is a list of available filenames : ${dataImages}`
+    );
+  } else if (isNaN(width && height)) {
+    res.status(400);
+    res.send(
+      "</span>Please enter a valid number for <b>height</b> and <b>width</b>, for example : <a href='images?filename=encenadaport&width=250&height=250'>api/images?filename=encenadaport&width=250&height=250</a></span>"
+    );
+  } else if (dataImages.includes(filename) && !isNaN(width && height)) {
+    try {
       await sharp(`${imagePath}/full/${filename}.jpg`)
         .resize({
           width,
           height,
         })
         .toFile(`${imagePath}/thumb/${filename}-${width}x${height}.jpg`);
+    } catch (error) {
+      console.log(`An error occurred during processing: ${error}`);
     }
-  } catch (error) {
-    console.log(`An error occurred during processing: ${error}`);
   }
 
-  images.get("/", (req, res) => {
-    res.sendFile(
-      `${path.resolve(__dirname, `../../../assets/images/`)}/thumb/${
-        req.query.filename
-      }-${width}x${height}.jpg`
-    );
-  });
+  res.sendFile(
+    `${path.resolve(__dirname, `../../../assets/images/`)}/thumb/${
+      req.query.filename
+    }-${width}x${height}.jpg`
+  );
+}
 
-  next();
-};
-
-images.use("/", resizeImage);
+images.get("/", resizeImage);
 
 export default images;
