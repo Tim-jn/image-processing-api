f3745761007028bc63c80fd64a7024f470bd0239
Tim <Tim280696@hotmail.com>
2022-07-01 14:59:17 +0200

    imageSpec: minor update

--- src/tests/routes/api/imageSpec.ts
+++ src/tests/routes/api/imageSpec.ts
@@ -1,21 +1,21 @@
-import request from "supertest";
-import { app } from "../../../index";
+import request from 'supertest'
+import { app } from '../../../index'
 
-describe("GET /api/images", () => {
-  describe("user enter a valid endpoint : /api/images?filename=encenadaport&width=250&height=250", () => {
-    it("should respond with a jpeg image", async () => {
+describe('GET /api/images', () => {
+  describe('user enter a valid endpoint : /api/images?filename=encenadaport&width=250&height=250', () => {
+    it('should respond with a jpeg image', async () => {
       const response = await request(app).get(
-        "/api/images?filename=encenadaport&width=250&height=250"
-      );
-      expect(response.headers["content-type"]).toBe("image/jpeg");
-    });
-  });
-  describe("user enter an invalid endpoint : /api/images?filename=encenadaport&width=250&height=250", () => {
-    it("should respond with error 404", async () => {
+        '/api/images?filename=encenadaport&width=250&height=250'
+      )
+      expect(response.headers['content-type']).toBe('image/jpeg')
+    })
+  })
+  describe('user enter an invalid endpoint : /api/images?filename=encenadaport&width=250&height=250', () => {
+    it('should respond with error 404', async () => {
       const response = await request(app).get(
-        "/api/images?filename=unknown&width=250&height=250"
-      );
-      expect(response.status).toBe(404);
-    });
-  });
-});
+        '/api/images?filename=unknown&width=250&height=250'
+      )
+      expect(response.status).toBe(404)
+    })
+  })
+})
