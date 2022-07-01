73f322d61dbc47a72fcc73310740933b30988b99
Tim <Tim280696@hotmail.com>
2022-07-01 15:58:17 +0200

    imageSpec: test if sharpResize works

--- src/tests/routes/api/imageSpec.ts
+++ src/tests/routes/api/imageSpec.ts
@@ -1,5 +1,7 @@
 import request from 'supertest'
 import { app } from '../../../index'
+import fs from 'fs'
+import sharpResize from '../../../utility/sharpResize'
 
 describe('GET /api/images', () => {
   describe('user enter a valid endpoint : /api/images?filename=encenadaport&width=250&height=250', () => {
@@ -8,6 +10,16 @@ describe('GET /api/images', () => {
         '/api/images?filename=encenadaport&width=250&height=250'
       )
       expect(response.headers['content-type']).toBe('image/jpeg')
+    })
+    it('should create a resized image', async () => {
+      const filepath = './assets/images/thumb/encenadaport-250x250.jpg'
+      if (fs.existsSync(filepath)) {
+        fs.unlinkSync(filepath)
+        console.log('File deleted')
+      }
+      await sharpResize('encenadaport', 250, 250)
+      expect(fs.existsSync(filepath)).toBeTruthy()
+      fs.unlinkSync(filepath)
     })
   })
   describe('user enter an invalid endpoint : /api/images?filename=encenadaport&width=250&height=250', () => {
