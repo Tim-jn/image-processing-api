897ed428a2671b9db8afc8c6662796428274d39d
Tim <Tim280696@hotmail.com>
2022-07-01 14:59:34 +0200

    sharpResize: create sharpResize

---
+++ src/utility/sharpResize.ts
@@ -0,0 +1,23 @@
+import sharp from 'sharp'
+import path from 'path'
+
+export default async function sharpResize(
+  filename: string,
+  height: number,
+  width: number
+) {
+  try {
+    await sharp(
+      `${path.resolve(__dirname, `../../assets/images/`)}/full/${filename}.jpg`
+    )
+      .resize(width, height)
+      .toFile(
+        `${path.resolve(
+          __dirname,
+          `../../assets/images/`
+        )}/thumb/${filename}-${width}x${height}.jpg`
+      )
+  } catch (error) {
+    console.log(`An error occurred during processing: ${error}`)
+  }
+}
