f2992f063b9388e8ab8b4f29a8a6d49df0a03bb0
Tim <Tim280696@hotmail.com>
2022-07-01 14:58:06 +0200

    index: minor update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import images from "./api/image";
-const routes = express.Router();
+import express from 'express'
+import images from './api/image'
+const routes = express.Router()
 
-routes.get("/", (req, res) => {
-  res.send("<p>There's nothing here.</p>");
-});
+routes.get('/', (req, res) => {
+  res.send("<p>There's nothing here.</p>")
+})
 
-routes.use("/images", images);
+routes.use('/images', images)
 
-export default routes;
+export default routes
