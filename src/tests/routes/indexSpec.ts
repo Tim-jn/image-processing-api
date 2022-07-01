4bbf499f40268f0e80bca16ba1018637aebd67dd
Tim <Tim280696@hotmail.com>
2022-07-01 14:59:08 +0200

    indexSpec: minor update

--- src/tests/routes/indexSpec.ts
+++ src/tests/routes/indexSpec.ts
@@ -1,9 +1,9 @@
-import request from "supertest";
-import { app } from "../../index";
+import request from 'supertest'
+import { app } from '../../index'
 
-describe("GET /", () => {
-  it("should return the /api endpoint", async () => {
-    const response = await request(app).get("/api");
-    expect(response.status).toBe(200);
-  });
-});
+describe('GET /', () => {
+  it('should return the /api endpoint', async () => {
+    const response = await request(app).get('/api')
+    expect(response.status).toBe(200)
+  })
+})
