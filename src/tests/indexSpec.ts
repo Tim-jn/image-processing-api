1afaa193fc708aed49c032010fd38ef18b242aee
Tim <Tim280696@hotmail.com>
2022-07-01 14:58:52 +0200

    indexSpec: minor update

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,13 +1,13 @@
-import request from "supertest";
-import { app } from "../index";
+import request from 'supertest'
+import { app } from '../index'
 
-describe("GET /", () => {
-  it("should return the / endpoint", async () => {
-    const response = await request(app).get("/");
-    expect(response.status).toBe(200);
-  });
-  it("should return the title and the description of the api", async () => {
-    const response = await request(app).get("/");
-    expect(response.text).toMatch("Welcome to the image processing API !");
-  });
-});
+describe('GET /', () => {
+  it('should return the / endpoint', async () => {
+    const response = await request(app).get('/')
+    expect(response.status).toBe(200)
+  })
+  it('should return the title and the description of the api', async () => {
+    const response = await request(app).get('/')
+    expect(response.text).toMatch('Welcome to the image processing API !')
+  })
+})
