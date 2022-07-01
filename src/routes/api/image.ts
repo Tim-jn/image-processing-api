e711a0903fd18de76ef4c767da41cb4f3476e832
Tim <Tim280696@hotmail.com>
2022-07-01 14:58:38 +0200

    image: create sharpResize() + check if file exist

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,46 +1,46 @@
-import express, { Request, Response } from "express";
-import sharp from "sharp";
-import path from "path";
-import { dataImages } from "../../index";
+import express, { Request, Response } from 'express'
+import path from 'path'
+import { dataImages } from '../../index'
+import sharpResize from '../../utility/sharpResize'
+import fs from 'fs'
 
-const images = express.Router();
+const images = express.Router()
 
 async function resizeImage(req: Request, res: Response): Promise<void> {
-  const width = parseInt(req.query.width as string);
-  const height = parseInt(req.query.height as string);
-  const filename = req.query.filename as string;
-  const imagePath = path.resolve(__dirname, `../../../assets/images/`);
+  const width = parseInt(req.query.width as string)
+  const height = parseInt(req.query.height as string)
+  const filename = req.query.filename as string
 
   if (filename === undefined || !dataImages.includes(filename)) {
-    res.status(404);
+    res.status(404)
     res.send(
       `</span>Input file is missing : please enter a valid <b>filename</b> !<br/> <br/>For example : <a href='images?filename=encenadaport&width=250&height=250'>api/images?filename=encenadaport&width=250&height=250</a></span><br/> <br/>Here is a list of available filenames : ${dataImages}`
-    );
+    )
   } else if (isNaN(width && height)) {
-    res.status(400);
+    res.status(400)
     res.send(
       "</span>Please enter a valid number for <b>height</b> and <b>width</b>, for example : <a href='images?filename=encenadaport&width=250&height=250'>api/images?filename=encenadaport&width=250&height=250</a></span>"
-    );
+    )
   } else if (dataImages.includes(filename) && !isNaN(width && height)) {
-    try {
-      await sharp(`${imagePath}/full/${filename}.jpg`)
-        .resize({
-          width,
-          height,
-        })
-        .toFile(`${imagePath}/thumb/${filename}-${width}x${height}.jpg`);
-    } catch (error) {
-      console.log(`An error occurred during processing: ${error}`);
+    if (
+      !fs.existsSync(
+        `${path.resolve(
+          __dirname,
+          `../../../assets/images/`
+        )}/thumb/${filename}-${width}x${height}.jpg`
+      )
+    ) {
+      await sharpResize(filename, height, width)
     }
+    res.sendFile(
+      `${path.resolve(
+        __dirname,
+        `../../../assets/images/`
+      )}/thumb/${filename}-${width}x${height}.jpg`
+    )
   }
-
-  res.sendFile(
-    `${path.resolve(__dirname, `../../../assets/images/`)}/thumb/${
-      req.query.filename
-    }-${width}x${height}.jpg`
-  );
 }
 
-images.get("/", resizeImage);
+images.get('/', resizeImage)
 
-export default images;
+export default images
